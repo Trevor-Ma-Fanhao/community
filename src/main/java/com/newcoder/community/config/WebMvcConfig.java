40488f78e435f56cb289d5647ef28e86777d3210
Trevor-Ma-Fanhao <932952939@qq.com>
2024-02-25 19:31:17 +0800

    2.42 检查登陆状态完成 第二章完成

--- src/main/java/com/newcoder/community/config/WebMvcConfig.java
+++ src/main/java/com/newcoder/community/config/WebMvcConfig.java
@@ -1,6 +1,7 @@
 package com.newcoder.community.config;
 
 import com.newcoder.community.controller.intercepter.AlphaIntercepter;
+import com.newcoder.community.controller.intercepter.LoginRequiredInterceptor;
 import com.newcoder.community.controller.intercepter.LoginTicketInterceptor;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Configuration;
@@ -15,6 +16,9 @@ public class WebMvcConfig implements WebMvcConfigurer {
     @Autowired
     private LoginTicketInterceptor loginTicketInterceptor;
 
+    @Autowired
+    private LoginRequiredInterceptor loginRequiredInterceptor;
+
     @Override
     public void addInterceptors(InterceptorRegistry registry) {
         registry.addInterceptor(alphaIntercepter)
@@ -22,6 +26,9 @@ public class WebMvcConfig implements WebMvcConfigurer {
                 .addPathPatterns("/register", "/login");
 
         registry.addInterceptor(loginTicketInterceptor)
+                .excludePathPatterns("/**/*.css", "/**/*.js", "/**/*.png", "/**/*.jpg", "/**/*.jpeg");
+
+        registry.addInterceptor(loginRequiredInterceptor)
                 .excludePathPatterns("/**/*.css", "/**/*.js", "/**/*.png", "/**/*.jpg", "/**/*.jpeg");
     }
 }
--- src/main/java/com/newcoder/community/controller/UserController.java
+++ src/main/java/com/newcoder/community/controller/UserController.java
@@ -1,5 +1,6 @@
 package com.newcoder.community.controller;
 
+import com.newcoder.community.annotation.LoginRequired;
 import com.newcoder.community.entity.User;
 import com.newcoder.community.service.UserService;
 import com.newcoder.community.util.CommunityUtil;
@@ -46,11 +47,13 @@ public class UserController {
     @Autowired
     private HostHolder hostHolder;
 
+    @LoginRequired
     @RequestMapping(path = "/setting", method = RequestMethod.GET)
         public String getSettingPage(){
         return "/site/setting";
     }
 
+    @LoginRequired
     @RequestMapping(path = "/upload", method = RequestMethod.POST)
     public String uploadHeader(MultipartFile headerImage, Model model){
         if(headerImage == null){
