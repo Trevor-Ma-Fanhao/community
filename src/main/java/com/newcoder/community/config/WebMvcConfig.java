def0736f7a8bd595969c2ac8aab5f44170a69ad2
Trevor-Ma-Fanhao <932952939@qq.com>
2024-02-25 13:02:38 +0800

    2.27 完成

---
+++ src/main/java/com/newcoder/community/config/WebMvcConfig.java
@@ -0,0 +1,27 @@
+package com.newcoder.community.config;
+
+import com.newcoder.community.controller.intercepter.AlphaIntercepter;
+import com.newcoder.community.controller.intercepter.LoginTicketInterceptor;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+public class WebMvcConfig implements WebMvcConfigurer {
+    @Autowired
+    private AlphaIntercepter alphaIntercepter;
+
+    @Autowired
+    private LoginTicketInterceptor loginTicketInterceptor;
+
+    @Override
+    public void addInterceptors(InterceptorRegistry registry) {
+        registry.addInterceptor(alphaIntercepter)
+                .excludePathPatterns("/**/*.css", "/**/*.js", "/**/*.png", "/**/*.jpg", "/**/*.jpeg")
+                .addPathPatterns("/register", "/login");
+
+        registry.addInterceptor(loginTicketInterceptor)
+                .excludePathPatterns("/**/*.css", "/**/*.js", "/**/*.png", "/**/*.jpg", "/**/*.jpeg");
+    }
+}
