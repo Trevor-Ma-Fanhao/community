8f8437cd64aca0650c8878806be162e9158b3103
Trevor-Ma-Fanhao <932952939@qq.com>
2024-03-02 22:17:53 +0800

    第五章完成

--- src/main/java/com/newcoder/community/config/WebMvcConfig.java
+++ src/main/java/com/newcoder/community/config/WebMvcConfig.java
@@ -3,6 +3,7 @@ package com.newcoder.community.config;
 import com.newcoder.community.controller.intercepter.AlphaIntercepter;
 import com.newcoder.community.controller.intercepter.LoginRequiredInterceptor;
 import com.newcoder.community.controller.intercepter.LoginTicketInterceptor;
+import com.newcoder.community.controller.intercepter.MessageInterceptor;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
@@ -19,6 +20,9 @@ public class WebMvcConfig implements WebMvcConfigurer {
     @Autowired
     private LoginRequiredInterceptor loginRequiredInterceptor;
 
+    @Autowired
+    private MessageInterceptor messageInterceptor;
+
     @Override
     public void addInterceptors(InterceptorRegistry registry) {
         registry.addInterceptor(alphaIntercepter)
@@ -30,5 +34,9 @@ public class WebMvcConfig implements WebMvcConfigurer {
 
         registry.addInterceptor(loginRequiredInterceptor)
                 .excludePathPatterns("/**/*.css", "/**/*.js", "/**/*.png", "/**/*.jpg", "/**/*.jpeg");
+
+        registry.addInterceptor(messageInterceptor)
+                .excludePathPatterns("/**/*.css", "/**/*.js", "/**/*.png", "/**/*.jpg", "/**/*.jpeg");
+
     }
 }
